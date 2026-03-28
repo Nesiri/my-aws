8c5c9a61d71c21df17c1f55186dffe7ac38b6bfc
Nesiri <nesruhussein57@gmail.com>
2026-03-27 21:23:10 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is AWS-S3 deployed react app/i);
   expect(linkElement).toBeInTheDocument();
 });
